a39f22e56c1159dc402affbb9a0e69a554b1f0e8
sergei-okon <sergei.okon@gmail.com>
2022-03-14 14:06:55 +0200

    Refactored WriterRepositoryImpl

--- src/main/resources/db.migration/sql/10_insertDataToWriters_PostsTable.sql
+++ src/main/resources/db.migration/sql/10_insertDataToWriters_PostsTable.sql
@@ -3,7 +3,3 @@ VALUES (1, 1),
        (1, 2),
        (2, 3),
        (3, 4);
-
-SELECT writers_posts.writer_id, writers_posts.post_id,  posts.content, posts.created, posts.updated FROM posts JOIN writers_posts ON writers_posts.post_id=posts.post_id
-
-SELECT writers_posts.writer_id, writers_posts.post_id, posts.post_id, content, posts.created, posts.updated FROM posts JOIN writers_posts ON writers_posts.post_id=posts.post_id"
